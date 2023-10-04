e34b4b935838c83016bbaeb04690993bb296f1ae
Karan Jagdale <karanjagdale1@gmail.com>
2023-10-04 00:26:29 -0700

    Added Q_learning function-Needs to be debugged

--- InvPend.py
+++ InvPend.py
@@ -5,29 +5,81 @@ import matplotlib.pyplot as plt
 g = 9.80665 #acceleration due to gravity
 m = 1    #mass of the bar
 l = 0.5      #length of the bar
-TauRange = 1  
-Ts = 0.05 #50 ms sample time
-k = 0.005 #damping constant
 pi = 3.1415926535
-print("Hey")
+Gamma = 0.9 #Discount factor
+
+TauRange = m*g*l/2*np.sin(pi/4)  #max torque is such that it balances the mass in 45 degree configuration 
+
+res = 3  #resolution of the control input or the totan number of discretized values of the control inpput
+
+ThetaRes = 101 #resolution of theta : creating the discrete state from the originally continouos state theta
+                #Odd number in the resolution will ensure that the theta for the vertically up position is included in the discrete state
+
+Ts = 0.05 #50 ms sample time
+
+k = 0.05 #damping constant
+
+ThetaDisc = np.linspace(0, 2*pi, ThetaRes)
+TauDisc = np.linspace(-TauRange, TauRange, res)
+
 def thetaDDot(Theta, Tau):
     return 1.5*g/l*np.sin(Theta) - Tau*3/m/l**2
 
-def reward(Theta):
-    return 1/(Theta + 0.001)
+def reward(Theta, Tau):
+    return 1/(Theta - pi + 0.001)
 
-def DynSS(y, t, m, l, k, Tau, g):
+def DynSS(y, t, m,l,k,Tau,g):
     Theta, Omega = y
     dydt = [Omega, -1.5*g/l*np.sin(Theta) - 3*Tau/m/l**2 - 3*k*Omega/m/l**2]
     return dydt
+y0 = [pi,0]
+Tau = 0
+sol = odeint(DynSS, y0, [0, Ts, 2*Ts], args=(m,l,k,Tau,g))
+print(sol[:,0])
+def nextState(y,Tau):
+    y0 = y
+    sol = odeint(DynSS, y0, [0, Ts], args=(m,l,k,Tau,g))
+    return sol[1,:]
+#a =nextState([0,pi],0)
+#print(a)
 
-y0 = [pi/2, 0]
-t= [0, 10, 100]
-Tau =0
-sol = odeint(DynSS, y0, t, args=(m,l,k,Tau,g))
-#print(type(sol))
-print('Karan')
+Q_new = np.zeros((ThetaRes, res))
+def Q_learning(Q_new):
+    Theta = pi
+    Omega = 0
+    Tau = 0
+    Iterations = 1000000 
+    eps = 0.1
+    TauInd = TauDisc.index(Tau)
+    for i in range(Iterations):
+        #First find the next state
+        Theta_n, Omega_n = nextState([Theta, Omega], Tau)
 
+        #Update the Q function
+        #First find the closest Theta value in the discretized theta to the current Theta and Theta_n
+        diff = np.absolute(ThetaDisc - Theta)
+        ThetaInd = np.argmin(diff)
+
+        diff = np.absolute(ThetaDisc - Theta_n)
+        Theta_nInd = np.argmin(diff)
+        
+
+        Q_new[ThetaInd, TauInd] = (1-eps)*Q_new(ThetaInd, TauInd) + eps*(reward(ThetaDisc(ThetaInd), Tau) + Gamma*Q_new[Theta_nInd, :].max())
+        #generate a random action for the next instant
+        TauInd = np.random.randint(res)
+
+Q_learning(Q_new)
+        
+        
+
+
+
+
+#sol = odeint(DynSS, y0, t, args=(m,l,k,Tau,g))
+
+
+
+'''
 plt.plot(t, sol[:, 0], 'b', label='theta(t)')
 
 plt.plot(t, sol[:, 1], 'g', label='omega(t)')
@@ -35,3 +87,4 @@ plt.legend(loc='best')
 plt.xlabel('t')
 plt.grid()
 plt.show()
+'''
--- temp.py
+++ temp.py
@@ -2,24 +2,13 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-def pend(y, t, b, c):
-    theta, omega = y
-    dydt = [omega, -b*omega - c*np.sin(theta)]
-    return dydt
+a = np.linspace(0,6.28,101)
+print(a)
 
-b = 0.25
-c = 5.0
-y0 = [np.pi - 0.1, 0.0]
+def temp():
+    print(a)
 
+temp()
 
-t = np.linspace(0, 10, 101)
-
-from scipy.integrate import odeint
-sol = odeint(pend, y0, t, args=(b, c))
-import matplotlib.pyplot as plt
-plt.plot(t, sol[:, 0], 'b', label='theta(t)')
-plt.plot(t, sol[:, 1], 'g', label='omega(t)')
-plt.legend(loc='best')
-plt.xlabel('t')
-plt.grid()
-plt.show()
+b = [2,4,1]
+print(b.index(4))
