d078768afc98f346df210566abc434e96a832bcd
Karan Jagdale <karanjagdale1@gmail.com>
2023-10-06 00:32:16 -0700

    completed the first version of the code--needs modifications

--- InvPend.py
+++ InvPend.py
@@ -8,16 +8,18 @@ l = 0.5      #length of the bar
 pi = 3.1415926535
 Gamma = 0.9 #Discount factor
 
-TauRange = m*g*l/2*np.sin(pi/4)  #max torque is such that it balances the mass in 45 degree configuration 
+TauRange = m*g*l  #max torque is twice of what is required to balances the mass in 90 degree configuration 
 
-res = 3  #resolution of the control input or the totan number of discretized values of the control inpput
+res = 31  #resolution of the control input or the totan number of discretized values of the control inpput. This value should ne odd number
 
-ThetaRes = 101 #resolution of theta : creating the discrete state from the originally continouos state theta
+ThetaRes = 201 #resolution of theta : creating the discrete state from the originally continouos state theta
                 #Odd number in the resolution will ensure that the theta for the vertically up position is included in the discrete state
 
 Ts = 0.05 #50 ms sample time
 
-k = 0.05 #damping constant
+k = 0.1 #damping constant
+
+ThetaTarget = pi #Angle at which we intend to stabilize
 
 ThetaDisc = np.linspace(0, 2*pi, ThetaRes)
 TauDisc = np.linspace(-TauRange, TauRange, res)
@@ -25,12 +27,12 @@ TauDisc = np.linspace(-TauRange, TauRange, res)
 def thetaDDot(Theta, Tau):
     return 1.5*g/l*np.sin(Theta) - Tau*3/m/l**2
 
-def reward(Theta, Tau):
-    return 1/(Theta - pi + 0.001)
+def reward(Theta, Tau, ThetaTarget):
+    return 1/(Theta - ThetaTarget + 0.001)
 
 def DynSS(y, t, m,l,k,Tau,g):
     Theta, Omega = y
-    dydt = [Omega, -1.5*g/l*np.sin(Theta) - 3*Tau/m/l**2 - 3*k*Omega/m/l**2]
+    dydt = [Omega, -1.5*g/l*np.sin(Theta) + 3*Tau/m/l**2 - 3*k*Omega/m/l**2]
     return dydt
 #y0 = [pi,0]
 # Tau = 0
@@ -44,17 +46,21 @@ def nextState(y,Tau):
 #print(a)
 
 Q_new = np.zeros((ThetaRes, res))
-def Q_learning(Q_new):
+def Q_learning(Q_new, ThetaTarget):
     Theta = pi
     Omega = 0
     Tau = 0
-    Iterations = 1000000 
+    Iterations = 5000000
     eps = 0.1
     TauInd = np.where(TauDisc == Tau)[0][0]
     ThetaHist = [Theta]
     for i in range(Iterations):
         #First find the next state
         Theta_n, Omega_n = nextState([Theta, Omega], Tau)
+        if Theta_n >= 2*pi:
+            Theta_n = Theta_n - 2*pi
+        elif Theta_n < 0:
+            Theta_n = Theta_n + 2*pi
         ThetaHist.append(Theta_n)
 
         #Update the Q function
@@ -66,33 +72,50 @@ def Q_learning(Q_new):
         Theta_nInd = np.argmin(diff)
         
 
-        Q_new[ThetaInd, TauInd] = (1-eps)*Q_new[ThetaInd, TauInd] + eps*(reward(ThetaDisc[ThetaInd], Tau) + Gamma*Q_new[Theta_nInd, :].max())
+        Q_new[ThetaInd, TauInd] = (1-eps)*Q_new[ThetaInd, TauInd] + eps*(reward(ThetaDisc[ThetaInd], Tau, ThetaTarget) + Gamma*Q_new[Theta_nInd, :].max())
         #generate a random action for the next instant
         TauInd = np.random.randint(res)
         Tau = TauDisc[TauInd]
+        
+
         Theta, Omega = Theta_n, Omega_n
         if i%100000 == 0:
-            print(i)
+            print(i/100000)
     return Q_new, ThetaHist
 
-Q, ThetaHist = Q_learning(Q_new)
-plt.plot(ThetaHist)        
+Q, ThetaHist = Q_learning(Q_new, ThetaTarget)
+#print(ThetaHist)
+# plt.plot(ThetaHist)  
+   
+# plt.show()
+
+# Controling pendulum using the learned Q-function
+Theta0 = 2
+Omega0 = 0
+
+# Want to stabilize the Theta at pi (Vertically inverted)
+
+simIter = 10000
+Theta, Omega = Theta0, Omega0
+ThetaSim = [Theta]
+for i in range(simIter):
+        
+    diff = np.absolute(ThetaDisc - Theta)
+    ThetaInd = np.argmin(diff)
+
+    #Finding optimal action for the current state
+
+    optTauInd = np.argmax(Q[ThetaInd, :])
+    optTau = TauDisc[optTauInd]
+    Theta_n, Omega_n = nextState([Theta, Omega], optTau)
+    if Theta_n >= 2*pi:
+        Theta_n = Theta_n - 2*pi
+    elif Theta_n < 0:
+        Theta_n = Theta_n + 2*pi
+    ThetaSim.append(Theta_n)
+    Theta, Omega = Theta_n, Omega_n
+
+plt.plot(ThetaSim)
 plt.show()
 
 
-
-
-
-#sol = odeint(DynSS, y0, t, args=(m,l,k,Tau,g))
-
-
-
-'''
-plt.plot(t, sol[:, 0], 'b', label='theta(t)')
-
-plt.plot(t, sol[:, 1], 'g', label='omega(t)')
-plt.legend(loc='best')
-plt.xlabel('t')
-plt.grid()
-plt.show()
-'''
